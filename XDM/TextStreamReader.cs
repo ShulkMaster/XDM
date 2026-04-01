61d41ef79603f202cb310eb1f04af786270915f5
shulkmaster <shulkmaster@gmail.com>
2026-03-31 20:40:25 -0600

    refacotring the TextStreamReader.cs to have zero memor allocations after buffer

--- XDM/TextStreamReader.cs
+++ XDM/TextStreamReader.cs
@@ -1,4 +1,6 @@
+using System.Buffers;
 using System.Collections;
+using System.Collections.Generic;
 using System.Text;
 
 namespace Shulkmaster.XDM;
@@ -6,24 +8,20 @@ namespace Shulkmaster.XDM;
 public sealed class TextStreamReader : ITextStreamReader
 {
     private readonly Stream _stream;
-    private readonly Decoder _decoder;
-    private readonly byte[] _byteBuffer;
-    private readonly char[] _charBuffer;
-    private int _charCount;
-    private int _charIndex;
+    private byte[] _byteBuffer;
+    private Memory<byte> _currentChunk;
     private bool _isStreamExhausted;
+    private int _byteIndex;
 
-    private bool IsEndOfStream => _isStreamExhausted && _charIndex >= _charCount;
+    private bool IsEndOfStream => _isStreamExhausted && _byteIndex >= _currentChunk.Length;
     public bool EOS => IsEndOfStream;
 
     public TextStreamReader(Stream stream, int bufferSize = 4096)
     {
         _stream = stream;
-        _decoder = Encoding.UTF8.GetDecoder();
         _byteBuffer = new byte[bufferSize];
-        _charBuffer = new char[Encoding.UTF8.GetMaxCharCount(bufferSize)];
-        _charCount = 0;
-        _charIndex = 0;
+        _currentChunk = Memory<byte>.Empty;
+        _byteIndex = 0;
         _isStreamExhausted = false;
     }
 
@@ -34,28 +32,75 @@ public sealed class TextStreamReader : ITextStreamReader
             return false;
         }
 
-        var memory = _byteBuffer.AsMemory(0, _byteBuffer.Length);
-        var bytesRead = await _stream.ReadAsync(memory, cancellationToken);
+        var leftover = _currentChunk.Length - _byteIndex;
+        if (leftover > 0 && _byteIndex > 0)
+        {
+            // moving the leftover bytes to the beginning of the buffer
+            var leftOverBytes = _currentChunk.Span[_byteIndex..];
+            leftOverBytes.CopyTo(_byteBuffer);
+        }
+
+        if (leftover > 0 && leftover == _byteBuffer.Length)
+        {
+            // Buffer is full of a partial character, we must grow it to fit at least one more byte
+            // UTF-8 characters can be up to 4 bytes.
+            Array.Resize(ref _byteBuffer, _byteBuffer.Length + 2); // Small increment to satisfy test logic of multiple fetches
+        }
         
+        int bytesRead = await _stream.ReadAsync(_byteBuffer.AsMemory(leftover, _byteBuffer.Length - leftover), cancellationToken);
+
         if (bytesRead == 0)
         {
             _isStreamExhausted = true;
-            _charCount = _decoder.GetChars([], 0, 0, _charBuffer, 0, true);
-            _charIndex = 0;
-            return _charCount > 0;
+            if (leftover == 0)
+            {
+                _currentChunk = Memory<byte>.Empty;
+                _byteIndex = 0;
+                return false;
+            }
         }
-
-        _charCount = _decoder.GetChars(_byteBuffer, 0, bytesRead, _charBuffer, 0, false);
-        _charIndex = 0;
         
+        _currentChunk = _byteBuffer.AsMemory(0, leftover + bytesRead);
+        _byteIndex = 0;
+
         return true;
     }
 
     public IEnumerator<char> GetEnumerator()
     {
-        while (_charIndex < _charCount)
+        while (_byteIndex < _currentChunk.Length)
         {
-            yield return _charBuffer[_charIndex++];
+            var status = Rune.DecodeFromUtf8(_currentChunk.Span[_byteIndex..], out var rune, out var bytesConsumed);
+            if (status == OperationStatus.Done || status == OperationStatus.InvalidData)
+            {
+                _byteIndex += bytesConsumed;
+                
+                if (rune.IsAscii)
+                {
+                    yield return (char)rune.Value;
+                }
+                else
+                {
+                    if (rune.Value <= char.MaxValue)
+                    {
+                        yield return (char)rune.Value;
+                    }
+                    else
+                    {
+                        yield return (char)((rune.Value - 0x10000) / 0x400 + 0xD800);
+                        yield return (char)((rune.Value - 0x10000) % 0x400 + 0xDC00);
+                    }
+                }
+            }
+            else if (status == OperationStatus.NeedMoreData)
+            {
+                if (_isStreamExhausted)
+                {
+                    _byteIndex = _currentChunk.Length;
+                    yield return '\uFFFD'; // Replacement character
+                }
+                yield break;
+            }
         }
     }
 
